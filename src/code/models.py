8567b088fdd8707b4a9eeec3fe107b8653b9b023
Raquel Carmo <raquelarscarmo@gmail.com>
2021-02-11 08:19:39 +0000

    Update models.py
    Added feature to make_ResNet() and make_MobileNet() to account for detection or categorisation

--- src/code/models.py
+++ src/code/models.py
@@ -8,42 +8,66 @@ from tensorflow.keras.layers import concatenate, Dense, GlobalAveragePooling2D
 from tensorflow.keras.optimizers import SGD, Adam
 from tensorflow.keras.metrics import CategoricalAccuracy, Precision, Recall, TruePositives, FalsePositives, TrueNegatives, FalseNegatives
 
-np.set_printoptions(precision=4)
 
-
-def make_ResNet(IMAGE_DIM_W, IMAGE_DIM_H, LEARNING_RATE):
+def make_ResNet(IMAGE_DIM_W, IMAGE_DIM_H, LEARNING_RATE, detection = False):
 
   resNet = ResNet50(weights='imagenet', include_top=False, input_shape=(IMAGE_DIM_W, IMAGE_DIM_H, 3))
   resNet_pool = GlobalAveragePooling2D()(resNet.output)
 
-  z = Dense(6, activation="softmax")(resNet_pool)
-  model = tf.keras.Model(inputs = resNet.input, outputs = z)
+  if detection:
+      z = Dense(1, activation="sigmoid")(resNet_pool)
+      model = tf.keras.Model(inputs = resNet.input, outputs = z)
 
-  # compile model
-  model.compile(optimizer = Adam(learning_rate = LEARNING_RATE), 
-                loss = "categorical_crossentropy",
-                metrics = [CategoricalAccuracy(name="accuracy"), 
-                           Precision(name="precision"), Recall(name="recall"), 
-                           TruePositives(name='tp'), FalsePositives(name='fp'),
-                           TrueNegatives(name='tn'), FalseNegatives(name='fn')])
+      # compile model
+      model.compile(optimizer = Adam(learning_rate = LEARNING_RATE), 
+                    loss = "binary_crossentropy",
+                    metrics = [BinaryAccuracy(name="accuracy"), 
+                              Precision(name="precision"), Recall(name="recall"), 
+                              TruePositives(name='tp'), FalsePositives(name='fp'),
+                              TrueNegatives(name='tn'), FalseNegatives(name='fn')])
+
+  else: # categorisation
+      z = Dense(6, activation="softmax")(resNet_pool)
+      model = tf.keras.Model(inputs = resNet.input, outputs = z)
+
+      # compile model
+      model.compile(optimizer = Adam(learning_rate = LEARNING_RATE), 
+                    loss = "categorical_crossentropy",
+                    metrics = [CategoricalAccuracy(name="accuracy"), 
+                              Precision(name="precision"), Recall(name="recall"), 
+                              TruePositives(name='tp'), FalsePositives(name='fp'),
+                              TrueNegatives(name='tn'), FalseNegatives(name='fn')])
   #model.summary()
   return model
 
 
-def make_MobileNet(IMAGE_DIM_W, IMAGE_DIM_H, LEARNING_RATE):
+def make_MobileNet(IMAGE_DIM_W, IMAGE_DIM_H, LEARNING_RATE, detection = False):
 
   mobileNet = MobileNetV2(weights='imagenet', include_top=False, input_shape=(IMAGE_DIM_W, IMAGE_DIM_H, 3))
   mobileNet_pool = GlobalAveragePooling2D()(mobileNet.output)
 
-  z = Dense(6, activation="softmax")(mobileNet_pool)
-  model = tf.keras.Model(inputs = mobileNet.input, outputs = z)
+  if detection:
+      z = Dense(1, activation="sigmoid")(mobileNet_pool)
+      model = tf.keras.Model(inputs = mobileNet.input, outputs = z)
 
-  # compile model
-  model.compile(optimizer = Adam(learning_rate = LEARNING_RATE), 
-                loss = "categorical_crossentropy",
-                metrics = [CategoricalAccuracy(name="accuracy"), 
-                           Precision(name="precision"), Recall(name="recall"), 
-                           TruePositives(name='tp'), FalsePositives(name='fp'),
-                           TrueNegatives(name='tn'), FalseNegatives(name='fn')])
+      # compile model
+      model.compile(optimizer = Adam(learning_rate = LEARNING_RATE), 
+                    loss = "binary_crossentropy",
+                    metrics = [BinaryAccuracy(name="accuracy"), 
+                              Precision(name="precision"), Recall(name="recall"), 
+                              TruePositives(name='tp'), FalsePositives(name='fp'),
+                              TrueNegatives(name='tn'), FalseNegatives(name='fn')])
+
+  else: # categorisation
+      z = Dense(6, activation="softmax")(mobileNet_pool)
+      model = tf.keras.Model(inputs = mobileNet.input, outputs = z)
+
+      # compile model
+      model.compile(optimizer = Adam(learning_rate = LEARNING_RATE), 
+                    loss = "categorical_crossentropy",
+                    metrics = [CategoricalAccuracy(name="accuracy"), 
+                              Precision(name="precision"), Recall(name="recall"), 
+                              TruePositives(name='tp'), FalsePositives(name='fp'),
+                              TrueNegatives(name='tn'), FalseNegatives(name='fn')])
   #model.summary()
   return model
